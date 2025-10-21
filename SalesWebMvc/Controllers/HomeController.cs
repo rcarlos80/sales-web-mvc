4953af97e4e6ad343b08a90bf5ec7fd81554f767
Carlos Rabelo <carlosrabelo.dev@gmail.com>
2025-10-21 17:06:27 -0300

    Mudando do VS2019 para VS2017

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
