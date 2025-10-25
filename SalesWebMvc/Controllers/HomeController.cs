a53f8b3f77538c77d8819d5846db12888f282112
Carlos Rabelo <carlosrabelo.dev@gmail.com>
2025-10-24 22:28:53 -0300

    SellersController

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace SalesWebMvc.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index() //ação default
         {
             return View();
         }
